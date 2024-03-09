0dd6a623f69f2b8438a495264c786a20e4e8c54e
Martien Verbruggen <martien.verbruggen@gmail.com>
2024-03-09 12:26:22 +0100

    Worked down to minimal example

--- enemy.gd
+++ enemy.gd
@@ -1,26 +1,14 @@
-class_name EnemyP
+class_name Enemy
 extends CharacterBody2D
 
 var health: int
-var speed: float
-var label: String
 
-static func new_enemy(name: String, speed := 50.0, health := 100) -> EnemyP:
+static func new_enemy(health := 50) -> Enemy:
 	# Unfortunately, we cannot use a preload at the moment. 
 	# I think this is a bug in Godot, because there is some circularity
 	# created by a preload that it doesn't seem to deal with well
 	var my_scene: PackedScene = load("res://enemy.tscn")
-	var new_enemy: EnemyP = my_scene.instantiate()
+	var new_enemy: Enemy = my_scene.instantiate()
 	new_enemy.health = health
-	new_enemy.speed = speed
-	new_enemy.label = name
 	return new_enemy
 
-func _ready():
-	%Label.text = label
-	position.x = randf_range(25, 1200)
-	position.y = randf_range(25, 800)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
--- world.gd
+++ world.gd
@@ -1,9 +1,10 @@
 extends Node2D
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for i in range(1, 10):
 		print("Adding Enemy %d" % i)
-		var enemy = Enemy.new_enemy("Enemy %d" % i)
+		var enemy = Enemy.new_enemy()
 		add_child(enemy)
+		enemy.position.x = randf_range(60, 1152 - 60)
+		enemy.position.y = randf_range(60, 648 - 60)
