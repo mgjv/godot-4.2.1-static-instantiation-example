3e528d37a7fb4d55711421a11fbee9c3ae215192
Martien Verbruggen <martien.verbruggen@gmail.com>
2024-03-11 10:39:55 +0100

    Changed comment on preload line

--- enemy.gd
+++ enemy.gd
@@ -4,9 +4,7 @@ extends CharacterBody2D
 var health: int
 
 static func new_enemy(health := 50) -> Enemy:
-	# Unfortunately, we cannot use a preload at the moment. 
-	# I think this is a bug in Godot, because there is some circularity
-	# created by a preload that it doesn't seem to deal with well
+	# Change `load` on the next line to `preload` to trigger the problem
 	var my_scene: PackedScene = load("res://enemy.tscn")
 	var new_enemy: Enemy = my_scene.instantiate()
 	new_enemy.health = health
